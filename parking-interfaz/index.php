997af32d5a307a23d6a51a8cdbd084282c49655d
Jesus Muriel <aprendiz.aguayo@aguayo.co>
2019-05-15 12:27:20 -0500

    Cambio en la pantalla principal

--- parking-interfaz/index.php
+++ parking-interfaz/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Parking-CEET</title>
+	<title>Par-King Revolution</title>
   <link rel="stylesheet" type="text/css" href="styles.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
  	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -80,7 +80,7 @@
   </form>
 
   <div class="btn-center">
-    <a href="" class="btn btn-default btn-rounded font-index"  data-toggle="modal" data-target="#darkModalForm">Bienvenido a parking CEET</a>
+    <a href="" class="btn btn-default btn-rounded font-index" id="BienPar"  data-toggle="modal" data-target="#darkModalForm">Bienvenido a Par-King Revolution</a>
   </div>
   	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
