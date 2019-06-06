9afe5e7045a8ce22aaacdab1fade06b07d15ab82
EdwinMuriel17 <41930192+EdwinMuriel17@users.noreply.github.com>
2019-06-06 18:02:21 -0500

    Add files via upload

--- parking-interfaz/menu2ejem.php
+++ parking-interfaz/menu2ejem.php
@@ -13,21 +13,13 @@
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/demo.css" />
 <link rel="stylesheet" type="text/css" href="css/component.css" />
+<link rel="stylesheet" type="text/css" href="estilos/estilos.css" />
 <script src="js/modernizr-custom.js"></script>
 </head>
 
 <body>	
 
 
-
-<?php
-		session_start();
-
-			if(!isset($_SESSION["usuario"]))
-			{
-				header("location:index.php");
-			}
-	?>
 
 
 	
@@ -39,7 +31,7 @@
 
 <div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="#page-buy">Contactenos</a></div>
 <div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="#page-blog">información</a></div>
-
+<img class="logopar" src="images/logopar2.png " alt="img01" width="20%"/>
 
 <div class="pages-nav__item pages-nav__item--social">	
 <!--<a class="link link--social link--faded" href="#"><i class="fa fa-twitter"></i><span class="text-hidden">Twitter</span></a>
@@ -55,60 +47,70 @@
 <div class="page" id="page-home">
 <!-- Blueprint header -->
 <header class="bp-header cf">
-<span class="bp-header__present">Par-King Revolution <span class="bp-tooltip bp-icon bp-icon--about" data-content="The Blueprints are a collection of basic and minimal website concepts, components, plugins and layouts with minimal style for easy adaption and usage, or simply for inspiration."></span></span>
-<h1 class="bp-header__title">Actualización</h1>
-<p class="bp-header__desc">En este segmento puedes actualizar los datos <a href="">clickeame perra
-</a></p>
+	<span class="bp-header__present">Par-King Revolution <span class="bp-tooltip bp-icon bp-icon--about" data-content="The Blueprints are a collection of basic and minimal website concepts, components, plugins and layouts with minimal style for easy adaption and usage, or simply for inspiration."></span></span>
+	<h1 class="bp-header__title">Actualización</h1>
+	<p class="bp-header__desc">En este segmento puedes actualizar los datos e información referente a los roles de la plataforma <a href="">clickeame</a></p>
+	<form name="registerform" id="registerform" action="actualizar.php" method="post">
+			<input class="datos"type="number" name="Documento"  placeholder="Documento de Identidad"><br /><br />
+			<input class="datos"type="text" name="Nombres"  placeholder="Nombre Completo"><br /><br />
+			<input class="datos"type="text" name="Correo"  placeholder="E-mail"><br /><br />
+			<input class="datos"type="text" name="Direccion"  placeholder="Direccion"><br /><br />
+			<input class="datos"type="text" name="Placa"  placeholder="Placa"><br /><br />
+			<input class="datos"type="text" name="Vehiculo"  placeholder="Tipo de vehiculo"><br /><br />
 
-<!--<nav class="bp-nav">
-<a class="bp-nav__item bp-icon bp-icon--prev" href="index.php" data-info="previous Blueprintsueprint"><span>Previous Blueprint</span></a>
-<!--a class="bp-nav__item bp-icon bp-icon--next" href="" data-info="next Blueprint"><span>Next Blueprint</span></a-
-<a class="bp-nav__item bp-icon bp-icon--drop" href="http://tympanus.net/codrops/?p=25311" data-info="back to the Codrops article"><span>back to the Codrops article</span></a>
-<a class="bp-nav__item bp-icon bp-icon--archive" href="http://tympanus.net/codrops/category/blueprints/" data-info="Blueprints archive"><span>Go to the archive</span></a>
-</nav>
--->
+			<input type="button" onclick="alert('Hello World!')" value="Click Me!">
+			<input type="submit" value="actualizar" class="button" name="btn-actualizar">
 </header>
-<img class="poster" src="images/logopar.png " alt="img01" />
+<img class="poster" src="images/actualizar.png " alt="img01" width="340rem"/>
 </div>
+
+
 <!-- /page -->
 
 <div class="page" id="page-docu">
 <header class="bp-header cf">
-<h1 class="bp-header__title">Busqueda</h1>
-<p class="bp-header__desc">Para busqueda del usuario <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Clik aqui</a></p>
-<p class="info">
-</p>
-<p 
+<h1 class="bp-header__title">Busqueda avanzada</h1>
+<Section class="uno">
+<p class="bp-header__desc"> Sobre esta pagina se puede realizar una busqueda profunda de todos los roles que se encuentran dentro de la plataforma, es decir, Se puede realizar una busqueda profunda de los datos que se necesiten.  <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Clik aqui</a></p>
+</Section>
+<p class="info"></p>
+<br>
+<br>
+<br>
+<p>	 
 <label for="user.pass"> Seleccionar<br>
-<select name="eleccionar" size="1">
-
-
-<option>Ticket</option>
-
-<option>Cedula</option>
-
-<option>Placa</option>
-
-<option selected>Fecha</option>
-
-</select>	
+	<select name="Seleccionar" size="1" width: 80%>
+		<option selected>seleccione un tipo de busqueda</option>
+		<option value="ticket">Ticket</option>
+		<option value="Cedula">Cedula</option>
+		<option value="Placa">Placa</option>
+		<option value="Fecha">Fecha</option>
+	</select>
+<br>
+<br>	
+	<input id="id_input" type="text">
+	<input type="submit">
 </p>
-
-
+<form action="/action_page.php">
+  <input type="date" name="bday">
+  <input type="submit">
+</form>
 </header>
-<img class="poster" src="images/6.jpg" alt="img06" style="width:100%" />
+	<img class="poster" src="images/busava.png" alt="Busqueda avanzada" style= />
 </div>
+
 
 <div class="page" id="page-manuals">
 <header class="bp-header cf">
-<h1 class="bp-header__title">Manuals</h1>
-<p class="bp-header__desc">Based on Ilya Kostin's Dribbble shot <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p>
+<h1 class="bp-header__title">Busqueda de cliente</h1>
+<p class="bp-header__desc">Aqui se pueden realizar busquedas respecto al cliente, se pueden obtener datos como: vehiculo ingresado, horarios dentro del establecimiento, etc. <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p>
 <p class="info">
 "When you adopt a vegan diet we make a connection, you don't go back, it is not a diet, it is a lifestyle." &mdash; Freelee Frugivore
 </p>
 </header>
-<img class="poster" src="images/2.jpg" alt="img02" />
+<img class="poster" src="images/buscl.png" alt="img02" />
 </div>
+
 <div class="page" id="page-software">
 <header class="bp-header cf">
 <h1 class="bp-header__title">Software &amp; Downloads</h1>
@@ -119,6 +121,7 @@
 </header>
 <img class="poster" src="images/3.jpg" alt="img03" />
 </div>
+
 <div class="page" id="page-custom">
 <header class="bp-header cf">
 <h1 class="bp-header__title">Customization &amp; Settings</h1>
@@ -129,6 +132,7 @@
 </header>
 <img class="poster" src="images/4.jpg" alt="img04" />
 </div>
+
 <div class="page" id="page-training">
 <header class="bp-header cf">
 <h1 class="bp-header__title">Training &amp; Learning Center</h1>
@@ -139,16 +143,20 @@
 </header>
 <img class="poster" src="images/5.jpg" alt="img05" />
 </div>
+
+
 <div class="page" id="page-buy">
 <header class="bp-header cf">
 <h1 class="bp-header__title">Where to buy</h1>
 <p class="bp-header__desc">Based on Ilya Kostin's Dribbble shot <a href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a></p>
 <p class="info">
-"When people ask me why I don't eat meat or any other animal products, I say, 'Because they are unhealthy and they are the product of a violent and inhumane industry.'" &mdash;
+"When people ask me why I don't eat meat or xany other animal products, I say, 'Because they are unhealthy and they are the product of a violent and inhumane industry.'" &mdash;
 </p>
 </header>
 <img class="poster" src="images/6.jpg" alt="img06" />
 </div>
+
+
 <div class="page" id="page-blog">
 <header class="bp-header cf">
 <h1 class="bp-header__title">Blog &amp; News</h1>
@@ -159,6 +167,7 @@
 </header>
 <img class="poster" src="images/1.jpg" alt="img01" />
 </div>
+
 <div class="page" id="page-contact">
 <header class="bp-header cf">
 <h1 class="bp-header__title">Contact</h1>
