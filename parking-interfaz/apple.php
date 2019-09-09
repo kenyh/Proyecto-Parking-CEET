9647eb363daefb259945f9f461ca8e1301d38c62
EdwinMuriel17 <41930192+EdwinMuriel17@users.noreply.github.com>
2019-09-09 18:26:12 -0500

    Add files via upload

--- parking-interfaz/apple.php
+++ parking-interfaz/apple.php
@@ -30,17 +30,17 @@
       {
         header("location:index.php");
       }
-?>
+?> 
 
 <header>
-<nav>
+<nav class="navegacion">
   <div class="menu-container">
     <!-- Menu -->
-    <ul class="menu">
-      <li class="menu-apple">
+    <ul class="menu col">
+      <!--<li class="menu-apple">
         <a class=""><img style="width: 50px; height: 60px" src="images/logopar2.png"></a>
-      </li>
-
+      </li>-->
+      
       <li class="menu-iPhone">
         <a href="historial.php">
           <span>Historial</span>
@@ -48,14 +48,19 @@
       </li>
 
       <li class="menu-mac">
-        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Usuarios PK</a>
+        <a class="dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Usuarios PK</a>
         <div class="dropdown-menu">
           <a class="dropdown-item" href="registrar.php" role="button">Registrar</a><br>
           <a class="dropdown-item" href="actualizar.php" role="button">Actualizar </a><br>
           <a class="dropdown-item" href="eliminar.php" role="button">Eliminar</a>
         </div>
       </li>
-
+      <li class="menu-iPhone">
+        <a href="new.php">
+          <span>Lista usuarios</span>
+        </a>
+      </li>
+      
       </li>
       <li class="menu-tv">
         <a href="tarifas.php">
@@ -63,41 +68,56 @@
         </a>
       </li>
        <li class="menu-tv">
-        <a href="ingresos.php">
+        <a href="tarifas.php">
           <span>Ingresos</span>
         </a>
       </li>
-
+      
       <li class="menu-tv">
         <a href="Manuales.php">
           <span>Manuales</span>
         </a>
       </li>
-
+      
       <li class="menu-home">
         <a href="apple.php">
-          <i class="fa fa-home fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-home " aria-hidden="true"></i>
         </a>
       </li>
-
+      
       <li class="menu-store">
-        <span class="navbar-text">
+        <a class="hov-let" style="color:5E7585" padding-bottom="20px" href="cerrar_sesion.php">
+          <i  class="fa fa-power-off" aria-hidden="true"></i>
           <?php
-          echo "<br><br>Usuario: ".$_SESSION["usuario"]." ";
+            echo "".$_SESSION["usuario"]." ";
           ?>
-          <br>
-          <a class="" href="cerrar_sesion.php"><img src="https://img.icons8.com/metro/26/000000/exit.png"></a>
-        </span>
+        </a>
       </li>
     </ul>
-
-    <!-- Search -->
-
   </div>
 </nav>
 </header>
-<div class="fade-screen"></div>
+<!--<div class="fade-screen"></div>
 <script src="js/apple.js"></script>
+-->
+<!--<div class="contenedor">
+  <form class="formu" action="ejemplo.php" method="get">
+    <h2 class="titulo">COLOMBIA ES PRUEBA</h2>
+    <p>Nombre: <br><input class="texto" type="text" name="nombre" size="40"></p>
+    <p>Año de nacimiento: <br><input type="number" name="nacido" min="1990" max="2020"></p>
+    <p>Sexo:
+      <input type="radio" name="hm" value="h"> Hombre
+      <input type="radio" name="hm" value="m"> Mujer
+    </p>
+    <p>
+      <input class="btn-sub" type="submit" value="Enviar">
+      <br>
+      <br>
+      <input class="btn-sub" type="reset" value="Borrar">
+    </p>
+  </form>
+</div>-->
+
 
 </body>
 </html>
